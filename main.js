c53d905db384b9de9c93e705868c5a2289aa9fee
uomma <marcelloleone975@gmail.com>
2023-12-11 19:20:45 +0100

    delete todo

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            newTodo: '',
+
             pageTitle: 'my to do list in vue.js ',
             todoList: [
                 'fare la spesa',
@@ -17,21 +17,9 @@ createApp({
     },
     methods: {
         deleteTodo(index) {
-            console.log(index);
-            console.log('mi stai cancellando')
-
-            this.todoList.splice(index, 1);
+            console.log('elimina')
+            this.todoList.splice(index, 1) 
         },
-
-        addTodo() {
-
-            console.log(this.newTodo);
-
-            if (this.newTodo.trim() !== '') {
-
-                this.todoList.push(this.newTodo);
-                this.newTodo = '';
-            }
-        }
     }
+
 }).mount('#app');
